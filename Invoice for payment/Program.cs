37db1fa6a24f9ba88f4e40ae0e29a82245f94255
Viktor <Cezn.Vi@gmail.com>
2022-12-18 17:24:52 +0200

    WIP

---
+++ Invoice for payment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Invoice_for_payment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
